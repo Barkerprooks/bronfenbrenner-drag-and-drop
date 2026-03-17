936a79825941084f7241517f8679f0aae6ee5075
BarkerProoks <jonparkerbrooks@gmail.com>
2026-03-17 00:20:29 -0500

    Progress

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
-class Line {
-    constructor(v2Initial, v2Final) {
-        this.v2Initial = v2Initial;
-        this.v2Final = v2Final;
+class AnswerBox extends HTMLElement {
+    constructor(text) {
+        super();
+
+        this.classList.add("answer");
+
+        this.textElement = document.createElement("span");
+        this.dragElement = document.createElement("span");
+
+        this.textElement.innerText = text;
+        this.dragElement.classList.add("dragfrom");
+        this.dragElement.innerHTML = "&#x25cb;";
+
+        this.appendChild(this.textElement);
+        this.appendChild(this.dragElement);
     }
 }
 
-class Viewport {
-    constructor(selector) {
-        this.element = document.querySelector(id);
-        this.questions = [];
+customElements.define("answer-box", AnswerBox);
+
+const shuffle = (xs) => {
+    for (let i = xs.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let tmp = xs[i];
+        xs[i] = xs[j];
+        xs[j] = tmp;
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const left = document.querySelector("#left");
+    const right = document.querySelector("#right");
+    let [leftList, rightList] = [[], []];
+
+    const categories = await fetch("/answers.json");
+
+    Object.entries(await categories.json()).forEach(([_, answerList]) => {
+        shuffle(answerList);
+
+        answerList.forEach(answer => {
+            const answerBox = new AnswerBox(answer);
+            if (rightList.length >= 0 && rightList.length < 8) {
+                rightList.push(answerBox);
+            } else if (leftList.length >= 0 && leftList.length < 8) {
+                leftList.push(answerBox);
+            } else {
+                Math.random() > 0.5 ? leftList.push(answerBox) : rightList.push(answerBox);
+            }
+        });
+
+        leftList.forEach(answer => left.appendChild(answer));
+        rightList.forEach(answer => right.appendChild(answer));
+    });
 
     const container = document.querySelector("#container");
     const circles = document.querySelectorAll(".circle");
@@ -28,22 +67,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const resetAllCircles = () => circles.forEach(circle => circle.classList.remove("circle-outline"));
 
+    const withinCircle = (rect) => {
+        const dx = rect;
+    };
+
     circles.forEach(circle => {
+        const shape = circle.getBoundingClientRect();
+        console.log(shape);
+
         circle.addEventListener("mouseover", event => {
             resetAllCircles();
+
             circle.classList.add("circle-outline");
             event.stopPropagation();
+        });
+
+        circle.addEventListener("mousemove", event => {
+            console.log(event);
         });
 
         circle.addEventListener("mouseleave", event => {
             resetAllCircles();
             event.stopPropagation();
         });
+
+        circle.addEventListener("mouseup", event => {
+            dragging.active = false;
+            event.stopPropagation();
+        });
     });
 
     container.addEventListener("mouseup", () => {
         dragging.active = false
-        line.setAttribute("stroke", "white");
+        line.setAttribute("stroke", "transparent");
         line.setAttribute("x2", dragging.origin.x);
         line.setAttribute("y2", dragging.origin.y);
         container.querySelectorAll(".dragfrom").forEach(dot => {
@@ -60,9 +116,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
             dot.innerHTML = "&#x25cf;";
 
-            dragging.origin = { x: dotBounds.x + dotBounds.width / 2, y: dotBounds.y + dotBounds.height / 2 };
+            dragging.origin = {
+                x: dotBounds.x + dotBounds.width / 2,
+                y: dotBounds.y + dotBounds.height / 2
+            };
 
-            line.setAttribute("stroke", "white");
+            line.setAttribute("stroke", "transparent");
             line.setAttribute("x1", dragging.origin.x);
             line.setAttribute("y1", dragging.origin.y);
             line.setAttribute("y2", dragging.origin.x);
